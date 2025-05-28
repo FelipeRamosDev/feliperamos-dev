2678f37df4b341f01140e0734b401898e8970e62
FelipeRamosDev <felipe@feliperamos.info>
2025-05-28 19:27:05 -0300

    [FRD-16] CTA Button
    ## [FRD-16] Description
    Created the CTA button

---
+++ src/components/buttons/CTAButton/index.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Button } from '@mui/material';
+import type { ButtonProps } from '@mui/material/Button';
+import { parseCSS } from '@/helpers';
+
+interface CTAButtonProps extends ButtonProps {
+   className?: string | undefined;
+   children?: React.ReactNode;
+}
+
+const CTAButton: React.FC<CTAButtonProps> = ({ className = '', children, ...props }) => {
+   return (
+      <Button
+         className={parseCSS(className, 'cta-button')}
+         {...props}
+      >
+         {children}
+      </Button>
+   );
+}
+
+export default CTAButton;
