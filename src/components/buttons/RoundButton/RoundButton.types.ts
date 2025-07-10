a9169513ad8a086c5d9aa3596a4c3c47f9e669d3
Felipe Ramos <49094475+FelipeRamosDev@users.noreply.github.com>
2025-07-09 23:00:05 -0300

    [FRD-61] PageSpeed issues (#51)
    ## [FRD-61] Description
    Implementing fixes for PageSpeed reported issues

--- src/components/buttons/RoundButton/RoundButton.types.ts
+++ src/components/buttons/RoundButton/RoundButton.types.ts
@@ -1,6 +1,7 @@
 import type { ButtonProps } from '@mui/material/Button';
 
 export interface RoundButtonProps extends Omit<ButtonProps, 'className'> {
+   title: string;
    children: React.ReactNode;
    className?: string | string[];
 }
