a4069825b1e50405eed00034d9ace3b59b8f8fc2
FelipeRamosDev <felipe@feliperamos.info>
2025-07-17 21:53:24 -0300

    Update page.tsx

--- src/app/admin/experience/[experience_id]/page.tsx
+++ src/app/admin/experience/[experience_id]/page.tsx
@@ -1,16 +1,15 @@
 import { ExperienceDetailsContent } from '@/components/content/admin/experience';
 import { AdminPageBase } from '@/components/layout';
 import { parseAcceptLanguage } from '@/helpers';
-import { useAjax } from '@/hooks/useAjax';
 import { ExperienceData } from '@/types/database.types';
 import { headers } from 'next/headers';
+import ajax from '@/hooks/useAjax';
 
 export default async function AdminExperiencePage({ params }: { params: { experience_id: string } }) {
    const { experience_id } = await params;
    const headersList = await headers();
    const acceptLanguage = headersList.get('accept-language');
    const detectedLang = parseAcceptLanguage(acceptLanguage);
-   const ajax = useAjax();
 
    try {
       const { data, success, message } = await ajax.get<ExperienceData>(`/experience/${experience_id}`);
