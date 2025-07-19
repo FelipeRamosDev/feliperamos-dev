7a8f8736212457c6c986cd1b672b53f0c1cc8429
FelipeRamosDev <felipe@feliperamos.info>
2025-07-19 01:13:24 -0300

    [FRD-92] Markdown Display
    ## [FRD-92] Description
    Introduced a new Markdown component with styling and type definitions for rendering markdown content. Updated ExperienceSetsSection to use the Markdown component for summary, description, and responsibilities fields, replacing plain paragraph tags. Also cleaned up related SCSS by removing redundant font-size styling.

--- src/components/common/index.tsx
+++ src/components/common/index.tsx
@@ -3,3 +3,5 @@ export { default as Container } from './Container/Container';
 export { default as Logo } from './Logo/Logo';
 export { default as SocialLinks } from './SocialLinks/SocialLinks';
 export { default as Spinner } from './Spinner/Spinner';
+export { default as Markdown } from './Markdown/Markdown';
+export { default as TableBase } from './TableBase/TableBase';
