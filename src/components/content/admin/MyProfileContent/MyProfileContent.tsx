dd8bccd05bb2ebb3cc6f9444aed093d8377cba00
FelipeRamosDev <felipe@feliperamos.info>
2025-07-27 19:26:22 -0300

    [FRD-116] User Profile - Read Page
    ## [FRD-116] Description
    Introduces an admin 'My Profile' page with user information and social links. Adds a user avatar image and updates the TopHeader to display the avatar for admin users. Refactors user type definitions from 'User' to 'UserData' and updates related components and services accordingly.

---
+++ src/components/content/admin/MyProfileContent/MyProfileContent.tsx
@@ -0,0 +1,84 @@
+'use client';
+
+import { Card, Container } from "@/components/common";
+import { CardProps } from "@/components/common/Card/Card.types";
+import { PageHeader } from "@/components/headers";
+import WidgetHeader from "@/components/headers/WidgetHeader/WidgetHeader";
+import { ContentSidebar } from "@/components/layout";
+import DataContainer from "@/components/layout/DataContainer/DataContainer";
+import { useAuth } from "@/services";
+import { Camera, GitHub, LinkedIn } from "@mui/icons-material";
+import Link from "next/link";
+import { Fragment } from "react";
+
+export default function MyProfileContent() {
+   const cardProps: CardProps = { padding: 'm' };
+   const { user } = useAuth();
+
+   return (
+      <div className="MyProfileContent">
+         <PageHeader
+            title="My Profile"
+            description="Manage your profile information and settings."
+         />
+
+         <Container>
+            <ContentSidebar>
+               <Fragment>
+                  <Card {...cardProps}>
+                     <WidgetHeader title="Account Data" />
+                     
+                     <DataContainer>
+                        <label>E-mail:</label>
+                        <Link href={`mailto:${user?.email}`}>{user?.email}</Link>
+                     </DataContainer>
+                     <DataContainer>
+                        <label>Phone Number:</label>
+                        <Link href={`tel:${user?.phone}`}>{user?.phone}</Link>
+                     </DataContainer>
+                  </Card>
+
+                  <Card {...cardProps}>
+                     <WidgetHeader title="Personal Data" />
+
+                     <DataContainer>
+                        <label>First Name:</label>
+                        <p>{user?.first_name}</p>
+                     </DataContainer>
+                     <DataContainer>
+                        <label>Last Name:</label>
+                        <p>{user?.last_name}</p>
+                     </DataContainer>
+                  </Card>
+               </Fragment>
+
+               <Fragment>
+                  <Card {...cardProps}>
+                     <DataContainer vertical>
+                        <label>User ID:</label>
+                        <p>{user?.id}</p>
+                     </DataContainer>
+                  </Card>
+
+                  <Card {...cardProps}>
+                     <WidgetHeader title="Social Links" />
+
+                     <DataContainer>
+                        <label><GitHub /></label>
+                        <Link href={user?.github_url || ''}>{user?.github_url}</Link>
+                     </DataContainer>
+                     <DataContainer>
+                        <label><LinkedIn /></label>
+                        <Link href={user?.linkedin_url || ''}>{user?.linkedin_url}</Link>
+                     </DataContainer>
+                     <DataContainer>
+                        <label><Camera /></label>
+                        <Link href={user?.portfolio_url || ''}>{user?.portfolio_url}</Link>
+                     </DataContainer>
+                  </Card>
+               </Fragment>
+            </ContentSidebar>
+         </Container>
+      </div>
+   );
+}
