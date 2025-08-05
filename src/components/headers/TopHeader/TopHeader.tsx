ad7b6cae36dfb6ef591831f933a89fd136571afd
FelipeRamosDev <felipe@feliperamos.info>
2025-08-04 23:37:46 -0300

    Update TopHeader.tsx

--- src/components/headers/TopHeader/TopHeader.tsx
+++ src/components/headers/TopHeader/TopHeader.tsx
@@ -3,12 +3,8 @@ import { Logo } from '@/components/common';
 import Link from 'next/link';
 import { TopHeaderProps } from './TopHeader.types';
 import Image from 'next/image';
-import { useAuth } from '@/services';
 
 export default function TopHeader({ adminMenus, fullwidth }: TopHeaderProps): React.JSX.Element {
-   const { user } = useAuth();
-   const avatarUrl = user?.avatar_url || '/images/user-avatar.jpg';
-
    return (
       <header className="TopHeader">
          <Container fullwidth={fullwidth}>
@@ -19,7 +15,7 @@ export default function TopHeader({ adminMenus, fullwidth }: TopHeaderProps): Re
             {adminMenus && (
                <Link className="user-avatar" href="/admin/my-profile">
                   <Image
-                     src={avatarUrl}
+                     src="/images/user-avatar.jpg"
                      alt="User Avatar"
                      width={40}
                      height={40}
