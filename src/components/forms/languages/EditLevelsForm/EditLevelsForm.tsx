9e95c3e7ce1ab6ba3d9f37adc501c51461026b90
FelipeRamosDev <felipe@feliperamos.info>
2025-08-09 01:00:31 -0300

    [FRD-144] Language Details - Edit
    ## [FRD-144] Description
    Introduced EditLanguageDetailsForm and EditLevelsForm components, allowing inline editing of language details and proficiency levels in LanguageDetailsContent. Added corresponding text resources for form labels and integrated edit mode toggling with EditButtons.

---
+++ src/components/forms/languages/EditLevelsForm/EditLevelsForm.tsx
@@ -0,0 +1,53 @@
+import { useLanguageDetails } from '@/components/content/admin/language/LanguageDetailsContent/LanguageDetailsContext';
+import { Form, FormSelect } from '@/hooks';
+import { useTextResources } from '@/services/TextResources/TextResourcesProvider';
+import texts from './EditLevelsForm.texts';
+import { languageLevels } from '@/app.config';
+import { FormValues } from '@/hooks/Form/Form.types';
+import { useAjax } from '@/hooks/useAjax';
+import { LanguageData } from '@/types/database.types';
+
+export default function EditLevelsForm() {
+   const language = useLanguageDetails();
+   const { textResources } = useTextResources(texts);
+   const ajax = useAjax();
+
+   const handleUpdate = async (values: FormValues) => {
+      try {
+         const updated = await ajax.patch<LanguageData>('/language/update', { language_id: language.id, updates: values });
+         if (!updated.success) {
+            throw updated;
+         }
+
+         window.location.reload();
+         return updated;
+      } catch (error) {
+         return error;
+      }
+   }
+
+   return (
+      <Form initialValues={{...language}} onSubmit={handleUpdate} editMode>
+         <FormSelect
+            fieldName="reading_level"
+            label={textResources.getText('EditLevelsForm.reading_level')}
+            options={languageLevels}
+         />
+         <FormSelect
+            fieldName="writing_level"
+            label={textResources.getText('EditLevelsForm.writing_level')}
+            options={languageLevels}
+         />
+         <FormSelect
+            fieldName="speaking_level"
+            label={textResources.getText('EditLevelsForm.speaking_level')}
+            options={languageLevels}
+         />
+         <FormSelect
+            fieldName="listening_level"
+            label={textResources.getText('EditLevelsForm.listening_level')}
+            options={languageLevels}
+         />
+      </Form>
+   );
+}
