36459960046193edcb37438c82d9065e3ada7d06
Felipe Ramos <49094475+FelipeRamosDev@users.noreply.github.com>
2025-08-09 18:27:41 -0300

    [FRD-147] Create Education - Page (#136)
    ## [FRD-147] Description
    Introduces a new admin page for creating education records, including localized text resources and a form component. The form supports fields for institution, field of study, degree, grade, description, and period, with submission handling and navigation upon creation.

---
+++ src/components/content/admin/education/index.tsx
@@ -0,0 +1,1 @@
+export { default as EducationCreateContent } from './EducationCreateContent/EducationCreateContent';
---
+++ src/components/forms/educations/index.tsx
@@ -0,0 +1,1 @@
+export { default as CreateEducationForm } from './CreateEducationForm/CreateEducationForm';
