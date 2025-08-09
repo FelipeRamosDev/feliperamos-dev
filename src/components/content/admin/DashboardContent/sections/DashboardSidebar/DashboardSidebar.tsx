109ebc1cec3fcb5b3a9bc20684ee1f2ac3243399
Felipe Ramos <49094475+FelipeRamosDev@users.noreply.github.com>
2025-08-08 21:39:21 -0300

    [FRD-142] Languages Widget (#130)
    ## [FRD-142] Description
    Introduced a new LanguagesWidget component with supporting types and text resources. Updated the admin dashboard sidebar to include the LanguagesWidget. Refactored imports for LanguageCreateContent and CreateLanguageForm to use new index files for cleaner imports.

--- src/components/content/admin/DashboardContent/sections/DashboardSidebar/DashboardSidebar.tsx
+++ src/components/content/admin/DashboardContent/sections/DashboardSidebar/DashboardSidebar.tsx
@@ -1,8 +1,9 @@
-import { CompaniesWidget, SkillsWidget } from '@/components/widgets';
+import { CompaniesWidget, LanguagesWidget, SkillsWidget } from '@/components/widgets';
 
 export default function DashboardSidebar(): React.ReactElement {
    return (<>
       <SkillsWidget className="sidebar-item" />
       <CompaniesWidget className="sidebar-item" />
+      <LanguagesWidget className="sidebar-item" />
    </>);
 }
---
+++ src/components/widgets/LanguagesWidget/LanguagesWidget.texts.ts
@@ -0,0 +1,11 @@
+import { TextResources } from "@/services";
+
+const textResources = new TextResources();
+
+textResources.create('LanguagesWidget.title', 'Languages');
+textResources.create('LanguagesWidget.title', 'Linguagens', 'pt');
+
+textResources.create('LanguagesWidget.addButton.title', 'Add Language');
+textResources.create('LanguagesWidget.addButton.title', 'Adicionar Idioma', 'pt');
+
+export default textResources;
