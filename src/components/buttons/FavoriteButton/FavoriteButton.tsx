7d264c22020d015afb6cb005fafc3450ff148bbb
Felipe Ramos <49094475+FelipeRamosDev@users.noreply.github.com>
2025-08-26 13:10:51 -0300

    [FRD-180] Favorite CV Feature (#156)
    ## [FRD-180] Description
    Introduced a reusable FavoriteButton component for toggling favorite status. Integrated the button into CVDetailsInfos to allow marking a CV as favorite, updated styles for proper layout, and extended CVData type with an is_favorite property.

---
+++ src/components/buttons/FavoriteButton/FavoriteButton.tsx
@@ -0,0 +1,35 @@
+import { Star, StarBorder } from '@mui/icons-material';
+import { FavoriteButtonProps } from './FavoriteButton.types';
+import { parseCSS } from '@/helpers/parse.helpers';
+import styles from './FavoriteButton.module.scss';
+import { useState } from 'react';
+import { Spinner } from '@/components/common';
+
+export default function FavoriteButton({ isFavorite, iconSize = 'medium', spinnerSize = '1.5rem', className, onClick = async () => {} }: FavoriteButtonProps) {
+   const [ loading, setLoading ] = useState<boolean>(false);
+   const Icon = isFavorite ? Star : StarBorder;
+
+   const handleClick = async () => {
+      try {
+         setLoading(true);
+         await onClick();
+      } catch (error) {
+         console.error(error);
+      } finally {
+         setLoading(false);
+      }
+   }
+
+   const classes = parseCSS(className, [
+      styles.FavoriteButton
+   ]);
+
+   if (loading) {
+      return <Spinner size={spinnerSize} />
+   }
+
+   return (
+      <Icon className={classes} fontSize={iconSize} onClick={handleClick} />
+   );
+}
+   
