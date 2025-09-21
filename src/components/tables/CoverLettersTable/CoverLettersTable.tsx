afb15432d78db8e4bcfed5d410ceca6f3922d65c
Felipe Ramos <49094475+FelipeRamosDev@users.noreply.github.com>
2025-09-20 22:05:59 -0300

    [FRD-198] Letters Search Listing (#173)
    ## [FRD-198] Description
    Introduced the useConverLetters hook for managing cover letter data, including fetch, create, update, and delete operations. Updated CoverLettersTable to use this hook for loading and displaying cover letters, and added supporting types for search parameters.

--- src/components/tables/CoverLettersTable/CoverLettersTable.tsx
+++ src/components/tables/CoverLettersTable/CoverLettersTable.tsx
@@ -1,10 +1,27 @@
-import { TableBase } from "@/components/common";
+import { TableBase } from '@/components/common';
+import useConverLetters from '@/hooks/useConverLetters/useConverLetters';
+import { useEffect, useRef } from 'react';
 
 export default function CoverLettersTable() {
+   const { coverLetters, fetchCoverLetters, loading } = useConverLetters({ query: {} });
+   const fetched = useRef<boolean>(false);
+
+   useEffect(() => {
+      if (fetched.current) {
+         return;
+      }
+
+      fetched.current = true;
+      fetchCoverLetters().catch((error) => {
+         console.error('Error fetching letters:', error);
+      });
+   }, []);
+
    return (
       <TableBase
-         items={[]}
-         loading={false}
+         items={coverLetters}
+         loading={loading}
+         noDocumentsText="No cover letters found."
          columnConfig={[
             { propKey: 'id', label: 'ID' },
             { propKey: 'subject', label: 'Subject' },
