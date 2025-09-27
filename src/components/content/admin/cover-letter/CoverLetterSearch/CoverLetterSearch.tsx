5bf6e564a950f1eda7d8d321ae4c0eab63de5bec
FelipeRamosDev <felipe@feliperamos.info>
2025-09-27 10:31:39 -0300

    Update CoverLetterSearch.tsx

--- src/components/content/admin/cover-letter/CoverLetterSearch/CoverLetterSearch.tsx
+++ src/components/content/admin/cover-letter/CoverLetterSearch/CoverLetterSearch.tsx
@@ -18,7 +18,7 @@ export default function CoverLetterSearch() {
                LinkComponent={Link}
                color="tertiary"
                title="Add New Cover Letter"
-               href="/admin/cover-letter/create"
+               href="#"
             >
                <Add />
             </RoundButton>
